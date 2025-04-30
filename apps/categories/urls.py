ca783cf1b051107b17c1416d0981f8282cceab64
Alexandre Lainer Carvalho Junior - 1292418427 <1292418427@ulife.com.br>
2025-04-30 11:17:27 -0300

    fc

---
+++ apps/categories/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'categories'
+
+urlpatterns = [
+    path('adicionar/', views.add_category, name='add_category'),
+    path('editar/<int:id_category>/', views.edit_category, name='edit_category'),
+    path('', views.list_categories, name='list_categories'),
+    path('excluir/<int:id_category>/', views.delete_category, name='delete_category'),
+
+]
